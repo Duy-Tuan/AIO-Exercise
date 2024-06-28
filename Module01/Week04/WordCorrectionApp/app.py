4389557b725080cf1b4545830c5300b9818a1c02
Duy Tuan <duytuan1810@gmail.com>
2024-06-28 20:59:19 +0700

    feature/M01W04 Add solution for module 1 week 4 exercise

---
+++ Module01/Week04/WordCorrectionApp/app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from lenvenshtein_distance import levenshtein_distance
+
+
+def load_vocab(file_path):
+    with open(file_path, 'rt') as f:
+        lines = f.readlines()
+
+    words = sorted(set([line.strip() for line in lines]))
+    return words
+
+
+vocabs = load_vocab(file_path="./vocab.txt")
+
+
+def main():
+    st.title("Word Correction using Levenshtein Distance")
+    word = st.text_input("Word: ")
+
+    if st.button("Compute"):
+        leven_distances = dict()
+        for vocab in vocabs:
+            leven_distances[vocab] = levenshtein_distance(word, vocab)
+
+        sorted_distances = dict(
+            sorted(leven_distances.items(), key=lambda item: item[1]))
+        correct_word = list(sorted_distances.keys())[0]
+        st.write("Correct word: ", correct_word)
+
+        col1, col2 = st.columns(2)
+        col1.write("Vocabulary: ")
+        col1.write(vocabs)
+
+        col2.write("Distances: ")
+        col2.write(sorted_distances)
+
+
+if __name__ == "__main__":
+    main()
